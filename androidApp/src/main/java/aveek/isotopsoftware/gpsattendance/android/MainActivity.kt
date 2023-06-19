e54d13bb04b169a4f4c859e9ef75047a7021d15b
Aveek <nasrat-sharif.aveek@electrolux.com>
2023-06-19 23:08:42 +0800

    Update UI

--- androidApp/src/main/java/aveek/isotopsoftware/gpsattendance/android/MainActivity.kt
+++ androidApp/src/main/java/aveek/isotopsoftware/gpsattendance/android/MainActivity.kt
@@ -1,13 +1,25 @@
 package aveek.isotopsoftware.gpsattendance.android
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import aveek.isotopsoftware.gpsattendance.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +31,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    Column(modifier = Modifier
+                        .padding(40.dp)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center) {
+                        TextField(value = "nsaveek@gmail.com", label = {Text("Registration")}, onValueChange = {} )
+                        Spacer(modifier = Modifier.size(16.dp))
+                        TextField(value = "123456", label = {Text("Password")}, onValueChange = {} )
+                    }
                 }
             }
         }
